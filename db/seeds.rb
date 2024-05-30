ed8c084898763831d5adf33eab0d1ce229af9e50
Marie D HUYVETTER <marie_dhuyvetter@yahoo.fr>
2024-05-30 14:27:28 +0200

    models restaurant and review

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+Restaurant.destroy_all
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category: Restaurant::CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
